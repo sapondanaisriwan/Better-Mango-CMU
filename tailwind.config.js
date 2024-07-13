ab01137b138cdba32ef840bf90f9d2f0263684bf
Adashima <sapondanaisriwan@gmail.com>
2024-07-14 03:42:30 +0700

    update: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const { nextui } = require("@nextui-org/react");
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./index.html"],
-  plugins: [],
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+  ],
+  darkMode: "class",
+  plugins: [nextui()],
 };
